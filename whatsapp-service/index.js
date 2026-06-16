86d7cd2a493f046f0d4c3fd32ff23bdeaf5dcae6
ApoloRamos77 <aerp77@gmail.com>
2026-06-16 16:33:56 -0500

    fix: Switch whatsapp-api to Debian to fix Puppeteer crash

--- whatsapp-service/index.js
+++ whatsapp-service/index.js
@@ -12,7 +12,6 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium-browser',
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
